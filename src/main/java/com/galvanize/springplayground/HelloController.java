64f43db3e5dfb31e197d7c8bf5daaf9f15e3c73f
Jason Taylor <taylorjason@users.noreply.github.com>
2020-07-20 10:59:56 -0400

    Add GET math/pi

--- src/main/java/com/galvanize/springplayground/HelloController.java
+++ src/main/java/com/galvanize/springplayground/HelloController.java
@@ -7,9 +7,9 @@ package com.galvanize.springplayground;
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String helloWorld() {
-        return "Hello from Spring!";
+    @GetMapping("/math/pi")
+    public String getPi() {
+        return "3.141592653589793";
     }
 
 }
